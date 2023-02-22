eeb89993179a2000a58543cfb13234fa27af64c0
nidhijagga <nidhijagga5239@gmail.com>
2023-02-22 16:34:18 +0530

    Task4: Creating Login Screen Frontend

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,16 +13,52 @@ exports.postUserSignUp = (req, res, next) => {
   User.findOne({ where: { email: email } })
     .then((user) => {
       if (user) {
-        res.send(
-          `<script>alert('This email is already taken. Please choose another one.'); window.location.href='/'</script>`
-        );
+        res
+          .status(409)
+          .send(
+            `<script>alert('This email is already taken. Please choose another one.'); window.location.href='/'</script>`
+          );
       } else {
         User.create({
           name: name,
           email: email,
           password: password,
         });
+        res
+          .status(200)
+          .send(
+            `<script>alert('User Created Successfully!'); window.location.href='/'</script>`
+          );
       }
     })
     .catch((err) => console.log(err));
+};
+
+exports.postUserLogin = (req, res, next) => {
+  const email = req.body.loginEmail;
+  const password = req.body.loginPassword;
+
+  User.findOne({ where: { email: email } }).then((user) => {
+    if (user) {
+      if (user.password == password) {
+        res
+          .status(200)
+          .send(
+            `<script>alert('Login Successful!'); window.location.href='/'</script>`
+          );
+      } else {
+        res
+          .status(401)
+          .send(
+            `<script>alert('Password Incorrect!'); window.location.href='/'</script>`
+          );
+      }
+    } else {
+      res
+        .status(404)
+        .send(
+          `<script>alert("User doesn't Exists!"); window.location.href='/'</script>`
+        );
+    }
+  });
 };
--- router/userRouter.js
+++ router/userRouter.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/userController");
 router.use(express.static("public"));
 
 router.get("/", userController.getLoginPage);
-
+router.post("/login", userController.postUserLogin);
 router.post("/signUp", userController.postUserSignUp);
 
 module.exports = router;
